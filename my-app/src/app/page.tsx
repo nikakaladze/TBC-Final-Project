1119bd23b1f5342efe8abb80901b68bd5c1cc77b
nikakaladze <nikakaladze89@gmail.com>
2025-02-12 02:33:42 +0400

    feat:finish Data fetch and displaying in home page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getCurrentSession } from "@/actions/auth";
 import Header from "@/components/layout/Header";
 import SalesCampaignBanner from "@/components/layout/SalesCampaignBanner";
+import ProductGrid from "@/components/product/page";
 import { getAllProducts } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
 import Image from "next/image";
@@ -8,12 +9,15 @@ import Image from "next/image";
 
 const Home = async () => {
     const {user} = await getCurrentSession();
+    const products = await getAllProducts();
+
 return(
     <div>
+        
         <SalesCampaignBanner />
 
         <section className="container mx-auto py-8">
-            {/* <ProductGrid /> */}
+            <ProductGrid  products={products}/>
 
         </section>
     </div>
