425a095fce5f0f5557478986a45468c2ae369d7e
nikakaladze <nikakaladze89@gmail.com>
2025-02-12 03:05:09 +0400

    chore:Add Search funcionality for Searching Items

--- my-app/src/sanity/lib/client.ts
+++ my-app/src/sanity/lib/client.ts
@@ -24,4 +24,23 @@ export const getAllCategories = async () => {
   const query = `*[_type == "productCategory"]`
   const categories = await sanityFetch({ query: query })
   return categories.data as ProductCategory[];
+};
+
+export const getCategoryBySlug = async (slug: string) => {
+  const query = `*[_type == "productCategory" && slug.current == $slug][0]`
+  const category = await sanityFetch({ query: query, params: { slug } });
+  return category.data as ProductCategory;
+};
+
+export const getProductsByCategorySlug = async (slug: string) => {
+  const query = `*[_type == "product" && references(*[_type == "productCategory" && slug.current == $slug][0]._id)]`
+  const products = await sanityFetch({ query: query, params: { slug } });
+  return products.data as Product[];
+};
+
+
+export const getProductById = async (id: string) => {
+  const query = `*[_type == "product" && _id == $id][0]`;
+  const product = await sanityFetch({ query: query, params: { id } });
+  return product.data as Product;
 }
