f3b394ad10ca4b97ef5b9a3ce9d2efe8c533f4cb
nikakaladze <nikakaladze89@gmail.com>
2025-02-11 20:02:09 +0400

    chore:add some funcionality for Login User

--- my-app/src/app/auth/sign-up/page.tsx
+++ my-app/src/app/auth/sign-up/page.tsx
@@ -1,15 +1,45 @@
-import { getCurrentSession } from '@/actions/auth';
+import { getCurrentSession, loginUser, registerUser } from '@/actions/auth';
+import SignUp from '@/components/auth/SignUp';
 import { redirect } from 'next/navigation';
-import React from 'react'
+import React from 'react';
+import zod from 'zod'
+
+const signUpSchema = zod.object({
+    email:zod.string().email(),
+    password:zod.string().min(8),
+
+})
 
 const SignUpPage = async () => {
     const {user} =  await getCurrentSession();
     if(user){
         return redirect("/")
     }
-  return (
-    <div>page</div>
-  )
+
+    const action = async (prevState:any,formData:FormData) => {
+        "use server";
+        const parsed =  signUpSchema.safeParse(Object.fromEntries(formData));
+
+        if(!parsed.success){
+            return{
+                message:"Invalid Form Data"
+            }
+        }
+        const {email,password} = parsed.data;
+        const {user,error} = await registerUser(email,password);
+        if(error){
+            return{message:error}
+        }else if(user){
+            await  loginUser(email,password),
+
+            await redirect("/"),
+            
+        }
+
+
+        
+    }
+  return <SignUp />
 }
 
 export default SignUpPage;
