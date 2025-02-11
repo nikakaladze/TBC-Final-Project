95c02da1a34e165e89d8d28c9175afad0124fd0f
nikakaladze <nikakaladze89@gmail.com>
2025-02-11 17:56:45 +0400

    chore:add some styling in header.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,13 +1,7 @@
 import Header from "@/components/layout/Header";
 import Image from "next/image";
 
-const AnnouncementBar: any = () => {
-  return(
-    <div className="w-full  bg-black py-2">
-      <div className="container mx-auto flex items-center justify-center"></div>
-    </div>
-  )
-}
+
 
 export default function Home() {
 
--- my-app/src/components/layout/Header.tsx
+++ my-app/src/components/layout/Header.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+const AnnouncementBar: any = () => {
+  return (
+    <div className="w-full  bg-black py-2">
+      <div className="container mx-auto flex items-center justify-center px-8">
+        <span className="text-center text-sm font-medium tracking-wide text-white">
+          FREE SHIPING ON ORDERS OVER 20.00$ X FREE RETURNS
+        </span>
+      </div>
+    </div>
+  );
+};
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header className="w-full sticky top-0 z-50">
+      <div className='w-full transform transition-transform duration-300 ease-in-out'>
+        <AnnouncementBar />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
