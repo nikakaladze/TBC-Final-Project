67b407a78078ebcc40c683e2700e58676e0059d0
nikakaladze <nikakaladze89@gmail.com>
2025-02-12 04:02:25 +0400

    chore:working Product_details_page

--- my-app/src/sanity/lib/client.ts
+++ my-app/src/sanity/lib/client.ts
@@ -43,4 +43,17 @@ export const getProductById = async (id: string) => {
   const query = `*[_type == "product" && _id == $id][0]`;
   const product = await sanityFetch({ query: query, params: { id } });
   return product.data as Product;
-}
+};
+
+export const searchProducts = async (searchQuery: string) => {
+  const query = `*[_type == "product" && (
+    title match "*" + $searchQuery + "*" ||
+    description match "*" + $searchQuery + "*" ||
+    category->title match "*" + $searchQuery + "*" ||
+    category->slug.current match "*" + $searchQuery + "*"
+  )]`
+
+  const products = await sanityFetch({ query: query, params: { searchQuery } });
+  return products.data as Product[];
+};
+
